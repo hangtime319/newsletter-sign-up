91896adfb44d73485ec14a1fd8683b06b9e76ead
hangtime319 <hangtime31@hotmail.com>
2024-06-29 15:22:51 -0300

    Finalizando projeto

--- main.js
+++ main.js
@@ -2,13 +2,34 @@ const cardForm = document.querySelector(".card-desktop");
 const cardSuccess = document.querySelector(".card-success");
 const form = document.querySelector("form");
 const btn = document.querySelectorAll(".btn");
-const email = document.querySelector("#email");
+const email = document.getElementById("email");
+const error = document.querySelector(".error");
 
-function formSuccess(e) {
+
+email.addEventListener("input", (e) => {
+    if(email.validity.valid) {
+        error.innerHTML = "";
+        error.className = "error";
+    }
+}, false,
+);
+
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    cardForm.classList.toggle('display-none');
-    cardSuccess.classList.toggle('display-none');
-}
+    if(email != '' && email.validity.valid) {
+        // error.className = "error";
+        cardForm.classList.toggle('display-none');
+        cardSuccess.classList.toggle('display-none');
+    } else {
+        error.innerHTML = 'Valid email required'
+        error.className = "error";       
+    }
+    }, false,
+);
 
-btn[0].addEventListener("click", formSuccess);
-btn[1].addEventListener("click", formSuccess);
+btn[1].addEventListener("click", function(e) {
+    cardForm.classList.toggle('display-none');
+    cardSuccess.classList.toggle("display-none");
+});
+
+
